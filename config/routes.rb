484c2bb530b39e7acfbc4a029d32595e358db520
Paidamoyo <ashgirlchapfuwa@gmail.com>
2013-11-27 14:02:37 +0200

    added users and microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,7 +10,7 @@ DemoApp::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    # get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
